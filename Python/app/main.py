5f20e3b61a6ac5e53a8956fbadc228b9e4da3c00
narudonS <36829216+a5710122@users.noreply.github.com>
2023-03-31 09:40:44 +0700

    Can predictions chord with wave file

--- Python/app/main.py
+++ Python/app/main.py
@@ -167,3 +167,112 @@ for i, (spectrogram, label_id) in enumerate(spectrogram_ds.take(n)):
     ax.axis('off')
 
 plt.show()
+
+def preprocess_dataset(files):
+    files_ds = tf.data.Dataset.from_tensor_slices(files)
+    output_ds = files_ds.map(
+        map_func=get_waveform_and_label,
+        num_parallel_calls=AUTOTUNE)
+    output_ds = output_ds.map(
+        map_func=get_spectrogram_and_label_id,
+        num_parallel_calls=AUTOTUNE)
+    return output_ds
+
+train_ds = spectrogram_ds
+val_ds = preprocess_dataset(val_files)
+test_ds = preprocess_dataset(test_files)
+
+batch_size = 64
+train_ds = train_ds.batch(batch_size)
+val_ds = val_ds.batch(batch_size)
+
+train_ds = train_ds.cache().prefetch(AUTOTUNE)
+val_ds = val_ds.cache().prefetch(AUTOTUNE)
+
+for spectrogram, _ in spectrogram_ds.take(2):
+    input_shape = spectrogram.shape
+print('Input shape:', input_shape)
+num_labels = len(chords)
+
+
+# Instantiate the `tf.keras.layers.Normalization` layer.
+# norm_layer = layers.Normalization()
+# Fit the state of the layer to the spectrograms
+# with `Normalization.adapt`.
+# norm_layer.adapt(data=spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+model = models.Sequential([
+    layers.Input(shape=input_shape),
+#     norm_layer,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.MaxPooling2D(2),
+    layers.Dropout(0.2),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(2),
+    layers.Dropout(0.2),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(num_labels, activation='softmax'),
+])
+
+model.summary()
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy'],
+)
+
+EPOCHS = 100
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=EPOCHS,
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+)
+
+metrics = history.history
+plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+plt.legend(['loss', 'val_loss'])
+plt.show()
+
+test_audio = []
+test_labels = []
+
+for audio, label in test_ds:
+    test_audio.append(audio.numpy())
+    test_labels.append(label.numpy())
+
+test_audio = np.array(test_audio)
+test_labels = np.array(test_labels)
+
+y_pred = np.argmax(model.predict(test_audio), axis=1)
+y_true = test_labels
+
+test_acc = sum(y_pred == y_true) / len(y_true)
+print(f'Test set accuracy: {test_acc:.0%}')
+
+confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
+plt.figure(figsize=(10, 8))
+sns.heatmap(confusion_mtx,
+            xticklabels=chords,
+            yticklabels=chords,
+            annot=True, fmt='g')
+plt.xlabel('Prediction')
+plt.ylabel('Label')
+plt.show()
+
+sample_file = test_files[140]
+
+sample_ds = preprocess_dataset([str(sample_file)])
+
+for spectrogram, label in sample_ds.batch(1):
+    prediction = model(spectrogram)
+    plt.bar(chords, tf.nn.softmax(prediction[0]))
+    plt.title(f'Predictions for "{chords[label[0]]}"')
+    plt.show()
+    
+model.save('../model/working/model_conv')
